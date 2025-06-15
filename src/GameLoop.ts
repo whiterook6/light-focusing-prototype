1fbe9bd942a5b29fe16c06163eeb1aa099790ce1
Tim Graboski <whiterook6@gmail.com>
2025-06-14 21:57:52 -0700

    Adding animation loop

---
+++ src/GameLoop.ts
@@ -0,0 +1,47 @@
+export type CallbackFN = (deltaTimeMS: number) => void;
+
+export class GameLoop {
+  private update: CallbackFN;
+  private paused: boolean = false;
+  private static tickLengthMs: number = 1000 / 60;
+  private intervalID?: NodeJS.Timeout;
+  
+  constructor(callback: CallbackFN){
+    this.update = callback;
+  }
+  
+  public start = () => {
+    if (this.isPaused() || !this.intervalID){
+      this.paused = false;
+      this.intervalID = setInterval(this.gameLoop, GameLoop.tickLengthMs);
+    }
+  }
+  
+  public stop = () => {
+    this.paused = true;
+    if (this.intervalID){
+      clearInterval(this.intervalID);
+      this.intervalID = undefined;
+    }
+  }
+  
+  public togglePause = (): void => {
+    if (this.isPaused()){
+      this.start();
+    } else {
+      this.stop();
+    }
+  }
+  
+  public isPaused = (): boolean => {
+    return this.paused;
+  }
+  
+  public gameLoop = (): void => {
+    if (this.isPaused()){
+      return;
+    }
+    
+    this.update(GameLoop.tickLengthMs);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("Hello world");
+import { GameLoop } from './GameLoop';
+
+const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+if (!canvas){
+  throw new Error("Cannot get canvas");
+}
+const ctx = canvas.getContext('2d');
+if (!ctx) {
+  throw new Error("Cannot get canvas context");
+}
+
+const resizeCanvas = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+};
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
+
+let age = 0;
+const gameLoop = new GameLoop((deltaTimeMs: number) => {
+  age += deltaTimeMs;
+  ctx!.fillStyle = 'lightblue';
+  ctx!.fillRect(0, 0, canvas.width, canvas.height);
+  ctx!.fillStyle = 'black';
+  ctx!.font = '30px Arial';
+  ctx!.textAlign = 'center';
+  ctx!.fillText(`${age.toFixed(2)}`, canvas.width / 2, canvas.height / 2);
+});
+
+window.addEventListener('keydown', (event) => {
+  // if space bar is pressed, toggle pause
+  if (event.code === 'Space') {
+    gameLoop.togglePause();
+  }
+})
+
+gameLoop.start();
